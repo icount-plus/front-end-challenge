57aa240c287263f8a5b629cc03fcf66ebd258604
henriquemoreiraa <riqimoreiraa@gmail.com>
2023-03-08 18:31:38 -0300

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <section className="spacer layer1 flex flex-col " />;
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          1: '#1F6FEB',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
