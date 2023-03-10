127beb137dd61250708597ae9999710079eaf55b
henriquemoreiraa <riqimoreiraa@gmail.com>
2023-03-10 15:19:45 -0300

    feat: responsive user input

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,12 +6,11 @@ function Button({ text, colors }) {
 
   return (
     <>
-      <button
-        type="button"
-        className={`tracking-wide font-lalezar text-sm sm:text-base ${bgColor} translate-x-1 border-b-4 ${borderColor} rounded-md px-2 py-1 mr-2 my-1 leading-4`}
+      <div
+        className={`btn tracking-wide font-lalezar text-sm sm:text-base ${bgColor} translate-x-1 border-b-4 ${borderColor} rounded-md px-2 py-1 mr-2 my-1 leading-4`}
       >
-        {text === 'FaSearch' ? <FaSearch /> : text}
-      </button>
+        {text || <FaSearch />}
+      </div>
       {/* bg-bgButtonBlue bg-bgButtonGreen bg-bgblue2 border-bdButtonBlue border-bdButtonGreen border-bgblue */}
     </>
   );
--- src/components/FilterContributors.jsx
+++ src/components/FilterContributors.jsx
@@ -1,26 +1,42 @@
+import { useState } from 'react';
 import Button from './Button';
 import Input from './Input';
 
 function FilterContributors() {
+  const [isSearch, setIsSearch] = useState(false);
+
   return (
     <div className=" flex items-center max-w-2xl w-full bg-bgGray rounded-t-lg rounded-tl-lg py-1 px-2">
-      <div className="flex w-full sm:w-2/4">
-        <Button
-          text="Top Contributors"
-          colors={['bgButtonBlue', 'bdButtonBlue']}
-        />
-        <Button
-          text="All Contributors"
-          colors={['bgButtonBlue', 'bdButtonBlue']}
-        />
-      </div>
-
-      <div className="hidden sm:w-2/4 sm:block">
+      {!isSearch && (
+        <div className="flex w-full sm:w-2/4">
+          <Button
+            text="Top Contributors"
+            colors={['bgButtonBlue', 'bdButtonBlue']}
+          />
+          <Button
+            text="All Contributors"
+            colors={['bgButtonBlue', 'bdButtonBlue']}
+          />
+        </div>
+      )}
+      <div
+        className={`${isSearch ? 'block' : 'hidden'} ${
+          isSearch && 'w-full'
+        } sm:w-2/4 sm:block`}
+      >
         <Input placeholder="Type a contributor name..." />
       </div>
-      <div className="sm:hidden">
-        <Button text="FaSearch" colors={['bgblue2', 'bgblue']} />
-      </div>
+      <button
+        type="button"
+        className="sm:hidden"
+        onClick={() => setIsSearch((val) => !val)}
+      >
+        {isSearch ? (
+          <Button text="X" colors={['bgblue2', 'bgblue']} />
+        ) : (
+          <Button colors={['bgblue2', 'bgblue']} />
+        )}
+      </button>
     </div>
   );
 }
