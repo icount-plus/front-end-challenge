17dcea9bc73ee7fd9b3080500fa9f79d86952f71
henriquemoreiraa <riqimoreiraa@gmail.com>
2023-03-13 16:07:53 -0300

    fix: search hook

--- src/hooks/useSearchContributor.jsx
+++ src/hooks/useSearchContributor.jsx
@@ -9,25 +9,30 @@ export default function useSearchContributor() {
   const { repoData } = useContext(RepositoryContext);
   const { setContributor } = useContext(ContributorsContext);
 
+  // eslint-disable-next-line consistent-return
   useEffect(() => {
     if (repoData && searchContributor) {
-      const source = axios.CancelToken.source();
+      let cancel;
       (async () => {
         try {
           const res = await api.get(
-            `https://api.github.com/search/commits?q=author:${searchContributor}+repo:${repoData?.full_name}&per_page=1`
+            `https://api.github.com/search/commits?q=author:${searchContributor}+repo:${repoData?.full_name}&per_page=1`,
+            {
+              // eslint-disable-next-line no-return-assign
+              cancelToken: new axios.CancelToken((c) => (cancel = c)),
+            }
           );
-          setContributor(res.data.items[0]);
+          setContributor(res.data);
         } catch (error) {
           if (axios.isCancel(error)) {
-            console.error('axios error: ', error.message);
+            cancel();
           } else {
             console.error(error);
           }
         }
       })();
       return () => {
-        source.cancel('Pervious request canceled');
+        cancel();
       };
     }
   }, [searchContributor]);
