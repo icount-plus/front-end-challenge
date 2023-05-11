a77b94b0f545c494a0fc5775a071e30d22e19f06
renanwilson <renan.alves.ra16@gmail.com>
2023-05-11 15:55:11 -0300

    fix: create conditional to search news

--- src/pages/NewsList/NewsList.tsx
+++ src/pages/NewsList/NewsList.tsx
@@ -3,9 +3,11 @@ import './styles.scss';
 import { NewsSearchList } from 'components/NewsSearchList/NewsSearchList';
 import { useParams } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { useSearchNewsListContext } from 'contexts/SearchNewsContextList';
 
 export default function NewsList() {
   const { search } = useParams();
+  const { searchNewsList } = useSearchNewsListContext();
   return (
     <>
       <HelmetProvider>
@@ -20,6 +22,11 @@ export default function NewsList() {
           <h1 className="container__section-title"> Resultados para {search}</h1>
         </section>
         <main className="container__main">
+          {searchNewsList?.response.docs.length ? (
+            <NewsSearchList />
+          ) : (
+            <h1 className="container__section-title">Não encontramos nada</h1>
+          )}
           <NewsSearchList />
         </main>
       </div>
